e0f4fe7f096976d73b76cf14674ee95cfb5f91fd
angelopoole <8thgenwolf@gmail.com>
2016-02-03 23:32:21 +0000

    3 paper problem

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ var divIds = [];
 var matches = 0;
 
 function fillBoard() {
-  var cards = ["rock", "scissors", "paper"]
+  console.log('HI');
+  var cards = ["rock", "scissors", "paper"];
   //TODO: randomly assign positions to the tiles on the screen call
   // addTileToBoard here
   // Hint: You can use a forEach or a for loop
-  for (var i=1; i>3; i++) {
-
-    addTileToBoard(cards[i])
+  for (var i=0; i<3; i++) {
+    console.log('HI3', i);
+    addTileToBoard(cards[i]);
   }
-
-};
+   console.log('HI2');
+}
 
 function shuffle(array) {
   //TODO: use this method to shuffle the array
@@ -31,42 +32,44 @@ function shuffle(array) {
   }
 
   return newArray;
-};
+}
 
 function addTileToBoard(element) {
   //TODO: randomly assign ids to the divs
-  var div =  "<div id=\"" + assignments +"\" class=\""+ element + " token\"><img src=\"rock.jpg\"></div>";
+  var rock =  "<div id=\"" + assignments +"\" class=\""+ element + " token\"><img src=\"rock.jpg\"></div>";
+  var scissors =  "<div id=1" + assignments +"\" class=\""+ element + " token\"><img src=\"scissors.jpg\"></div>";
+  var paper =  "<div id=2" + assignments +"\" class=\""+ element + " token\"><img src=\"paper.jpeg\"></div>";
   $(".game").append(div);
-  assignments ++
-};
+  assignments ++;
+}
 
 function clearBoard() {
   $(".game").empty();
-};
+}
 
 function checkTilesMatch(array) {
   //TODO: check to see if the value of the 2 face up tile match
 
-};
+}
 
 function setPoint() {
     $('#userPoint').text(userPoint);
-};
+}
 
 function evaluate( evt ) {
     var tileType = evt.target.getAttribute('class');
     var divID = evt.target.getAttribute("id");
     //TODO: If this is first tile clicked store the card if it is the second tile
     // clicked check to see if it is the same as the first.
-};
+}
 
 
- $(document).ready(function() {
-  console.log("this is running");
+$(document).ready(function() {
+ console.log("this is running");
   fillBoard();
   function bop(elm) {
       $(elm).fadeOut('fast').delay(1).fadeIn('fast');
-  };
+  }
 
   
 
